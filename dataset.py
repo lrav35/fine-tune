6b779157fb4f76e901f594365e873c5e01024171
Luc Ravenelle <lrav35@gmail.com>
2023-12-18 09:37:35 -0500

    dataset skeleton

---
+++ dataset.py
@@ -0,0 +1,20 @@
+import typing
+import math
+import os
+from torch.utils.data import DataSet
+
+
+class MyDataSet(DataSet):
+    """torch dataset"""
+    def __init__():
+        return None
+
+    """get dataset length"""    
+    def __len__(self) -> int:
+       return None
+
+    """get an item from dataset given an index"""
+    def __getitem__(self, idx: int):
+        super(MyDataSet, self).__init__()
+        return None
+        
