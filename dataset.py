ab803fd1e66b7d478a5d71e0a888fa76ac9399c2
Luc Ravenelle <lrav35@gmail.com>
2024-01-02 10:44:41 -0500

    adding quick updates

--- dataset.py
+++ dataset.py
@@ -1,20 +1,39 @@
-import typing
+from typing import Dict
+import torch
 import math
 import os
+import transformers
+import datasets
 from torch.utils.data import DataSet
 
 
 class MyDataSet(DataSet):
-    """torch dataset"""
-    def __init__():
-        return None
+    """Dataset for fine-tuning model"""
 
-    """get dataset length"""    
-    def __len__(self) -> int:
-       return None
-
-    """get an item from dataset given an index"""
-    def __getitem__(self, idx: int):
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer, paths: str, limit=None):
         super(MyDataSet, self).__init__()
+        dataset = (
+            datasets.load_dataset(
+            "json",
+            data_files=paths,
+            split=split=f"train[0:{limit}]" if limit else "train",
+            )
+            .filter(
+                # filter data entries
+                )
+            .map(
+                # create a preprocessing function 
+            )
+        )
+
+        self.tokenizer = tokenizer
+        self.data = None 
+        # self.size = len(dataframe)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        
         return None
         
